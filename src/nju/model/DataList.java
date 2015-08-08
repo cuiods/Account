ea515b0b19180df43409a9019b73ebd0c7c2bd75
cuiods <chao14@software.nju.edu.cn>
2015-08-08 12:54:10 +0800

    解决一个严重的bug
    data文件里有内容了 之前总是抛异常 现在可以正常读取了  recordVO怎么改我再想想吧

--- src/nju/model/DataList.java
+++ src/nju/model/DataList.java
@@ -11,6 +11,7 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import nju.model.po.RecordKind;
 import nju.model.po.RecordPO;
 import nju.model.po.UserPO;
 import nju.model.vo.RecordVO;
@@ -24,18 +25,19 @@ public class DataList {
 	
 	public DataList() {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(DATAPATH));
-			if(br.readLine()!=null){
-				br.close();
-				reader = new ObjectInputStream(new FileInputStream(DATAPATH));
-				Object record = null;
-				while((record = reader.readObject())!=null){
-					records.add((RecordPO)record);
+			//BufferedReader br = new BufferedReader(new FileReader(DATAPATH));
+			//if(br.readLine()!=null){
+				//br.close();
+				FileInputStream fr = new FileInputStream(DATAPATH);
+				reader = new ObjectInputStream(fr);
+				while(fr.available()>0){
+					RecordPO record = (RecordPO) reader.readObject();
+					records.add(record);
 				}
 				reader.close();
-			}else{
-				br.close();
-			}
+			//}else{
+				//br.close();
+			//}
 		} catch (FileNotFoundException e) {
 			System.out.println("Error: can't find data file");
 			e.printStackTrace();
@@ -91,4 +93,40 @@ public class DataList {
 		}
 		return recordView;
 	}
+	
+	public ArrayList<RecordPO> getRecords(){
+		return records;
+	}
+	
+//	public static void main(String[] args){
+//		DataList list = new DataList();
+//		UserPO user = new UserPO("aa aa", "aa");
+//		Calendar c = Calendar.getInstance();
+//		RecordPO record1 = new RecordPO(user, RecordKind.CATERING, c, -100);
+//		RecordPO record2 = new RecordPO(user, RecordKind.ENTERTAINMENT, c, -100);
+//		RecordPO record3 = new RecordPO(user, RecordKind.TRANSFERACOUNTS, c, 1000);
+//		RecordPO record4 = new RecordPO(user, RecordKind.TRANSPORTATION, c, -100);
+//		RecordPO record5 = new RecordPO(user, RecordKind.WAGE, c, 100);
+//		list.addRecord(record1);
+//		list.addRecord(record2);
+//		list.addRecord(record3);
+//		list.addRecord(record4);
+//		list.addRecord(record5);
+//		list.saveData();
+//		try {
+//			ObjectInputStream input = new ObjectInputStream(new FileInputStream("data/data.seq"));
+//			RecordPO record1 = (RecordPO)input.readObject();
+//			RecordPO record2 = (RecordPO)input.readObject();
+//			RecordPO record3 = (RecordPO)input.readObject();
+//			RecordPO record4 = (RecordPO)input.readObject();
+//			RecordPO record5 = (RecordPO)input.readObject();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 }
